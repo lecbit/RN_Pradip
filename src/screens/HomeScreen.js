a4bb934b6bc394ce11b34f1173d437cfaff7a090
lecbit <artm-marych@rambler.ru>
2023-04-24 10:25:57 +0100

    ~4~ Tabs

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { SafeAreaView, ScrollView, View, Text, Image, ImageBackground, TextInput, TouchableOpacity } from "react-native";
 import FeatherIcon from "react-native-vector-icons/Feather";
 import Carousel from 'react-native-snap-carousel';
 import { sliderData } from "../model/data";
 import BannerSlider from "../components/BannerSlider";
 import { windowWidth } from "../utils/Dimensions";
+import CustomSwitch from "../components/CustomSwitch";
 
 export default function HomeScreen() {
+    const [gamesTab, setGamesTab] = useState(1);
     const renderBanner = ({ item, index }) => {
         return ( //<BannerSlider data={item}/>
             <View>
@@ -16,6 +18,10 @@ export default function HomeScreen() {
                 />
             </View>
         )
+    }
+
+    const onSelectSwitch = (value) => {
+        setGamesTab(value);
     }
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
@@ -59,6 +65,11 @@ export default function HomeScreen() {
                     itemWidth={300}
                     loop={true}
                 />
+                <View style={{ marginVertical: 20 }}>
+                    <CustomSwitch selectionMode={1} option1="Free" option2="Paid" onSelectSwitch={onSelectSwitch} />
+                </View>
+                {gamesTab == 1 && <Text>Free Games</Text>}
+                {gamesTab == 2 && <Text>Paid Games</Text>}
             </ScrollView>
         </SafeAreaView>
     )
